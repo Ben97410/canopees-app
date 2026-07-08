0b508822f9152093bdf3e90f7868a052dbc89ed7
Benoit <benoit.clain974@gmail.com>
2026-07-08 14:43:01 +0200

    ajout du nom utilisateur lors du login lors de la connection du backend

--- canopees-backend/src/Entity/User.php
+++ canopees-backend/src/Entity/User.php
@@ -58,7 +58,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     
     public function __toString(): string
     {
-        return ($this->prenom ?? '') . ' ' . ($this->nom ?? 'Utilisateur sans nom');
+        return ($this->prenom ?? '') . ' ' . ($this->nom ??'');
     }
 
     
